71cacf7bb7a577610fbf7e12f633ee4e1b22bca3
Janek1010 <janekdomozych99@gmail.com>
2024-03-04 14:12:38 +0100

    javascript done

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -142,3 +142,192 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+
+// DESTRUCTURING
+/*
+const books = getBooks();
+
+const book  = getBook(2);
+
+// const title = book.title;
+// const author = book.author;
+// nie jest to takie fajne bo duzo wywolan byloby potrzebnych z dupensa
+// musi byc taka sama nazwa jak w obiekcie, tzn. ze np titles nie  zadziala
+
+const {title,author,pages,publicationDate,genres,hasMovieAdaptation} = book;
+
+//gorsza opcja
+// const primaryGenre = genres[0];
+// const secondaryGenre = genres[1];
+
+// to wezmie nam pierwszy i drugi element z genres, a ten kropkowy to wezmie nam reszte wartosci z genres
+const [primaryGenre,secondaryGenre, ...otherGenres] = genres; 
+
+// REST/SPREAD Operator
+
+const newGenres = ['epic fantasy', ...genres] // tak fajnie bo nam jakby wezmie te elementy z genres da nam tu i mamy tablice genres + 1 nowy
+// inaczej bylaby lipa jak np dalbym genres bo bylaby tablica w tablicy 
+// to akurat moze byc z przodu lub z tylu to ...genres
+
+// to nizej jakby doda nowe pole moviPoblicationDate oraz nadpisze ilosc pages, jesli natomiast dam pages na poczatku to book nadpisze nasza wartosc
+const updatedBook = {
+  ...book,
+  
+  // Adding new property
+  moviePublicationDate: '2001-12-19',
+
+  // Overwriting existing property 
+  pages: 1210
+} // tu tez bez tych kropek mialbym obiekt book + pole a tam to mam wszystkie pola w jednym obiekcie jakby
+
+// TEMPLATE LITERALS
+const getYear = (str) => str.split("-")[0];
+
+const summary = `${title}, a ${pages}-pages long book, was written by ${author} and published in ${getYear(publicationDate)}
+. The book has${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
+summary;
+
+// TENARIES INSTEAD OF IF/ELSE STATEMENTS
+
+const pageRange = pages > 1000 ? 'over a thousand' : 'less than 1000';
+pageRange;
+console.log(`The book has ${pageRange} pages`)
+
+// ARROW FUNCTIONS
+
+// tam wyzej jest
+
+// SHORT-CIRCUTING AND LOGICAL OPERATORS: &&, ||, ??
+
+console.log(true && "Some String")
+console.log(false && "Some String")
+
+console.log(hasMovieAdaptation && "This book has a movie"); // fajna opcja 
+
+// falsy: 0,'',null,undefined
+console.log('jonas' && 'Some string');
+console.log(0 && "Some string");
+
+console.log(true || "Some String");
+console.log(false || "Some String");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "NOT  TRANSLATED";
+spanishTranslation;
+
+// tu przyklad gdzie trzeba uwazac bo jest 0 reviews a pokaze no data
+console.log(book.reviews.librarything.reviewsCount);
+const countWrong = book.reviews.librarything.reviewsCount || 'no data';
+countWrong;
+
+// to jest rozwiazanie -  zwraca druga wartosc jesli pierwsza to null or undefined, ale nie kiedy jest 0 lub empty string
+const count = book.reviews.librarything.reviewsCount ?? "no data";
+count;
+
+function getTotalReviewCount(book){
+  const goodRead = book.reviews?.goodreads?.reviewsCount;
+  // ten znak robi optionala ze jesli nie istnieje to nie bedzie probowal dostac sie dalej
+  // a te 2 znaki zapytania to to co wczesniej ze wtedy 0 nam wpisze ziomal
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0; 
+  return goodRead + librarything;
+}
+
+console.log(getTotalReviewCount(book));
+
+
+// THE ARRAY MAP METHOD
+const books = getBooks();
+
+// dla kazdego elementu robi operacje w nawiasach
+const x = [1,2,3,4,5].map((el) => el *2 );
+console.log(x);
+
+const titles = books.map((book)=> book.title);
+titles;
+
+function getTotalReviewCount(book){
+  const goodRead = book.reviews?.goodreads?.reviewsCount;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0; 
+  return goodRead + librarything;
+}
+
+const essentialData = books.map(book=>({
+    title: book.title,
+    author: book.author,
+    reviewsCount: getTotalReviewCount(book)
+  }));
+essentialData;
+
+// THE ARRAY FILTER METHOD
+
+const longBooksWithMovie = books
+  .filter(book=>book.pages > 500)
+  .filter(book=>book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter(book=>book.genres.includes("adventure"))
+  .map(book=>book.title);
+
+adventureBooks;
+
+// THE ARRAY REDUCE METHOD
+
+const pagesAllBooks = books.reduce((sum,book)=> sum+book.pages, 0);
+pagesAllBooks;
+
+// THE ARRAY SORT METHOD
+
+const arr = [3,7,1,9,6];
+const sorted = arr.slice().sort((a,b) => a-b); // b-a to rosnaca , to posortuje nam ta wczesniejsza tablice dlatego slice robimy 
+sorted;
+arr;
+
+const sortedByPages = books.slice().sort((a,b)=>b.pages-a.pages);
+
+// WORKING WITH IMMUTABLE ARRAYS
+
+// 1) Add book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Potter and Chamber of Secrets",
+  author: " J. K. Rowling"
+};
+
+const booksAfterAdd = [...books,newBook];
+booksAfterAdd
+
+
+// 2) Delete book object from array
+const booksAfterDelete = booksAfterAdd.filter(book => book.id !== 3);
+booksAfterDelete
+
+// 3) Update book object on the array
+const booksAfterUpdate = booksAfterDelete.map(book=>book.id === 1 ? {...book, pages: 1210} : book);
+
+booksAfterUpdate
+*/
+
+// ASYNCHRONOUS JAVASCRIPT: PROMISES
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then(res=>res.json())
+//   .then(data=> console.log(data));
+
+// console.log("jonas");
+
+// ASYNCHRONOUS JAVASCRIPT: ASYNC/AWAIT
+
+async function getTodos(){
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json()
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos)
+
+console.log("jonas")
